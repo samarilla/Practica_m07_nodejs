179d394e63380a003bdd1b70e5bb4c713bffb5bc
Sergio Amarilla <samarilla@ilg.cat>
2022-05-12 20:20:00 +0200

    Devuelve datos JSON

--- app.js
+++ app.js
@@ -4,3 +4,23 @@ const port = 3000;
 
 app.get('/', (req, res) => res.send('Hello World!'));
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+
+app.get("/products", (req,res) => {
+  const products = [
+  {
+    id: 1,
+    name: "hammer",
+  },
+  {
+    id: 2,
+    name: "screwdriver",
+  },
+  ,
+  {
+    id: 3,
+    name: "wrench",
+  },
+ ];
+
+ res.json(products);
+});
