7f3c92ad6758556592aa8ef56da85753e9d86b82
Sergio Amarilla <samarilla@ilg.cat>
2022-05-13 09:59:12 +0200

    Primera parte acabado

--- app.js
+++ app.js
@@ -1,26 +1,30 @@
-const express = require('express');
-const app = express();
-const port = 3000;
+const express = require('express')
+function isAuthorized(req,res, next) {
+  const auth = req.headers.authorization;
+  if (auth === 'secretpassword') {
+    next();
+  } else {
+    res.status(401);
+    res.send('Not permitted');
+  }
+}
+const app = express()
+const port = 3000
 
-app.get('/', (req, res) => res.send('Hello World!'));
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.get('/', (req, res) => res.send('Hello World!'))
 
-app.get("/products", (req,res) => {
-  const products = [
-  {
+app.get('/users', isAuthorized, (req,res) => {
+  res.json([{
     id: 1,
-    name: "hammer",
-  },
-  {
-    id: 2,
-    name: "screwdriver",
-  },
-  ,
-  {
-    id: 3,
-    name: "wrench",
-  },
- ];
+    name: 'User Userson'
+  }])
+ })
 
- res.json(products);
-});
+app.get('/products', (req, res) => {
+  res.json([{
+    id: 1,
+    name: 'The Bluest Eye'
+  }])
+})
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
